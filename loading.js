b0487a5a7bcbbe0367aada4ed96bfe6309af5677
Danielle Barker <caseybarker86@gmail.com>
2021-06-23 19:22:19 -0600

    cleaning up fcerda.html

---
+++ loading.js
@@ -0,0 +1,8 @@
+var redirect = "./Portfolio_English/index.html";
+setTimeout(() => {
+  let video = document.getElementById("video");
+  video.style.visibility = "hidden";
+  setTimeout(() => {
+    window.location.href = redirect;
+  }, 7000);
+}, 18000);
