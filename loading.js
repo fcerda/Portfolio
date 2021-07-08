45f4bb6e8bf9edf7ea2454ba312e949eba069c23
Fernando A Cerda <fcerda@users.noreply.github.com>
2021-07-07 23:00:39 -0600

    last

--- loading.js
+++ loading.js
@@ -1,9 +1,22 @@
 var count = 5;
 var redirect = "./Portfolio_English/index.html";
-setTimeout(() => {
+const skipVideo=()=>{
+  let video = document.getElementById("video");
+  video.style.visibility = "hidden";
+  clearTimeout(videoTimer)
+  let text = document.getElementsByTagName("p");
+ for (line of text){
+   line.classList.add("cancelVideo")
+ }
+ setTimeout(() => {
+  window.location.href = redirect;
+}, 5000);
+}
+const videoTimer = () => {
   let video = document.getElementById("video");
   video.style.visibility = "hidden";
   setTimeout(() => {
     window.location.href = redirect;
   }, 7000);
-}, 18000);
+}
+setTimeout(videoTimer, 18000);
